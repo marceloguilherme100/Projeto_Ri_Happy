c1d8f2b9a46e35450c9a7b7cbf5a64f8a4e56135
marceloguilherme100 <marcelo.guilherme100@gmail.com>
2024-10-25 17:52:51 -0300

    atulizando jogo

--- Jogo Ri Happy/src/scripts/engine.js
+++ Jogo Ri Happy/src/scripts/engine.js
@@ -1,9 +1,51 @@
 const state = {
     view: {
-    squares: document.querySelectorAll(".square")
-    
+    squares: document.querySelectorAll(".square"),
+    enemy: document.querySelector(".enemy"),
+    timeLeft: document.querySelector("#time-left"),
+    score: document.querySelector("#score")
 },  
 
-values: {},
+values: {
+    timerId: null,
+    gameVelocity: 1000, 
+    hitPosition: 0,
+    result: 0,
+
+},
 };
 
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy")
+    state.values.hitPosition = randomSquare.id;
+
+}
+
+function moveEnemy() {
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+}
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square) => {
+       square.addEventListener("mousedown", () => {
+        if(square.id === state.values.hitPosition) {
+            state.values.result++
+            state.view.score.textContent = state.values.result
+        }
+       });
+    });
+}
+
+function initialize() {
+    
+    moveEnemy();
+    addListenerHitBox();
+}
+
+initialize();
